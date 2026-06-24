39850cdc60fb2f1f1c73f0c93c543eb0a13facf1
Darli963 <dmedinas5@upao.edu.pe>
2026-06-24 00:31:49 -0500

    Revert "Develop"

--- api-sistema-cotizacion/backend/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from "./app";
-import { config } from "./config/env";
-
-app.listen(config.port, "0.0.0.0", () => {
-  console.log(`[server] running on http://0.0.0.0:${config.port}`);
-});
--- terraform/shared/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  repository_name = "Infra_Proyecto"
-  managed_by      = "terraform"
-  owner           = "infra-team"
-}
